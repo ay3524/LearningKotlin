a47c583046f39e1138bc0db72ca45f21dac7a06d
Ashish <ashish@smartbeings.ai>
2018-10-13 01:01:34 +0530

    Collection code added.

--- src/8KT.kt
+++ src/8KT.kt
@@ -1,4 +1,14 @@
 //Fixed size collections
 fun main(args: Array<String>) {
+    val array1 = arrayOf(1, 2, 3, 4, 5)
+    for (array in array1) {
+        println(array)
+    }
 
+    val nullsArray = arrayOfNulls<String>(3)
+    for (array in nullsArray) {
+        println(array)
+    }
+
+    //You can perform operation like sort(), sortArrayDescending()
 }
